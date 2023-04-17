82b7754dbef08ceffb1a95b916536ea6e75d4335
Matheus Borges <127253921+matheusborgesm@users.noreply.github.com>
2023-04-17 18:36:01 -0300

    Create Clientes.java
    Clientes

---
+++ src/Clientes.java
@@ -0,0 +1,53 @@
+public class Clientes {
+
+	public class Cliente implements Comparable<Cliente> {
+		
+		private static final String CARGO = "CLIENTE";
+		private String titular;
+		private String senha;
+		private String cpf;
+		
+		public Cliente() {
+		}
+		
+		public Cliente(String nome, String senha, String cpf) {
+			this.titular = nome;
+			this.senha = senha;
+			this.cpf = cpf;
+		}
+		
+		public void setTitular(String titular) {
+			this.titular = titular;
+		}
+
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+
+		public String getNome() {
+			return titular;
+		}
+
+		public String getSenha() {
+			return senha;
+		}
+
+		public String getCpf() {
+			return cpf;
+		}
+
+		public static String getCargo() {
+			return CARGO;
+		}
+		
+		@Override
+		public int compareTo(Cliente o) {
+			 return this.getNome().compareTo(o.getNome());
+			 
+		}
+	}	
+}
