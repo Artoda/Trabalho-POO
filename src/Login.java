ea21cb30a62d93fb0679ce14e90c1e4064854dd9
Matheus Borges <127253921+matheusborgesm@users.noreply.github.com>
2023-04-18 17:32:24 -0300

    Create Login.java
    Login

---
+++ src/Login.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Login {
+
+	private static final Scanner entrada = new Scanner(System.in);
+    private static final PrintStream saida = System.out;
+
+	public static void loginCliente(Map<String, Cliente> mapaContas, Map<String, Funcionario> mapaFuncionario) throws IOException {
+		
+		do {
+			saida.print("Digite seu CPF: ");
+			String login = entrada.nextLine();
+			saida.print("Digite sua senha: ");
+			String senha = entrada.nextLine();
+			
+				if(mapaContas.containsKey(login) && mapaContas.get(login).getSenha().equals(senha)) {
+					Conta usuarioLogado = (Conta) mapaContas.get(login);
+					
+					if(usuarioLogado.getTipo() == TipoEnum.CORRENTE.name()) {
+						MenuContaCorrente.menuContaCorrente(usuarioLogado, mapaContas, mapaFuncionario);
+						break;
+					} 
+					if(usuarioLogado.getTipo() == TipoEnum.POUPANCA.name()) {
+						MenuContaPoupanca.menuContaPoupanca(usuarioLogado, mapaContas, mapaFuncionario);
+						break;
+					}
+				
+				} else {
+					System.out.println("Login ou senha inv�lidos.");
+			}	
+		} while (true);			 	
+	}
+	
+	public static void loginFuncionario(Map<String, Cliente> mapaContas, Map<String, Funcionario> mapaFuncionario) throws IOException { 
+		Scanner in = new Scanner (System.in); 
+		
+		do {
+			saida.print("Digite seu CPF:") ;
+			String login = in.nextLine(); 
+			saida.print("Digete sua senha: ") ;
+			String senha = in.nextLine(); 
+			Funcionario usuarioLogado = mapaFuncionario.get(login);
+			
+			if(mapaFuncionario.containsKey(login) && senha.equals(usuarioLogado.getSenha())) { 
+				usuarioLogado.menuFuncionario(usuarioLogado, mapaContas, mapaFuncionario);
+				break;
+			} else {
+				System.out.println ("Login ou senha inválidos! Tente novamente");
+			}
+		}while (true);
+	}
+}
