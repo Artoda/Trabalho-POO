7b656196dea612a7d3976beeb888ca27e67fcfd1
João Marco Piccoli Cardoso <127253938+JoaoMarcoPiccoliCardoso@users.noreply.github.com>
2023-04-18 19:23:41 -0300

    Sistema

--- src/sistemaBancario/SistemaBancario.java
+++ src/sistemaBancario/SistemaBancario.java
@@ -1,72 +1,91 @@
 package sistemaBancario;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
-import conta.ContasEnum;
+import operador.CargosEnum;
+import operador.Pessoa;
+import operador.Funcionario;
 import operador.Diretor;
 import operador.Gerente;
 import operador.Presidente;
+import operador.Cliente;
+import conta.ContasEnum;
+import conta.Conta;
+import conta.Agencia;
+import conta.ContaCorrente;
+import conta.ContaPoupanca;
+
 
 public class SistemaBancario {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Bem-vindo ao sistema banc�rio!");
+	public static void main(String[] args) {
+    	
+    	//Scanner read = new Scanner(System.in);
+    	
+    	Cliente cliente1 = new Cliente("Romulo", "123.456.789-00", 1234);
+    	Cliente cliente2 = new Cliente("João", "123.456.789-01", 1234);
+    	
+    	Agencia agencia = new Agencia(1234);
+    	
+    	Funcionario gerente = new Gerente("Victor", "123.456.789-02", 1234, agencia);
+    	
+    	Funcionario diretor = new Diretor("Matheus", "123.456.789-03", 1234);
+    	
+    	Funcionario presidente = new Presidente("Yan", "123.456.789-04", 1234);
+    	
+    	Conta corrente = new ContaCorrente(1234, 0.0, cliente1);
+    	Conta poupanca = new ContaPoupanca(1234, 0.0, cliente2);
+    	
+    	agencia.adicionarConta(corrente);
+    	agencia.adicionarConta(poupanca);
+    	
+    	
+    	String cpf;
+    	int senha;
+    	
+    	menu();
 
-        //  ler arquivo de texto com dados dos clientes, contas e funcion�rios
-
-        boolean sair = false;
-        while (!sair) {
-            System.out.println("Por favor, digite seu CPF: ");
-            String cpf = scanner.nextLine();
-            System.out.println("Digite sua senha: ");
-            String senha = scanner.nextLine();
-            //public HashMap<String, ContasEnum> listaCliente = new HashMap<String, ContasEnum>();
-            // Verifica se � um Cliente
-            // implementar a l�gica de verifica��o de Cliente
-            // Verifica se � um Gerente
-            
-            Gerente gerente = verificarGerente(cpf, senha);
-            if (gerente != null) {
-                System.out.println("Bem-vindo, gerente da ag�ncia " + gerente.agencia);
-                // implementar as opera��es dispon�veis para o Gerente
-                continue;
-            }
-
-            // Verifica se � um Diretor
-            Diretor diretor = verificarDiretor(cpf, senha);
-            if (diretor != null) {
-                System.out.println("Bem-vindo, diretor!");
-                // implementar as opera��es dispon�veis para o Diretor
-                continue;
-            }
-
-            // Verifica se � um Presidente
-            Presidente presidente = verificarPresidente(cpf, senha);
-            if (presidente != null) {
-                System.out.println("Bem-vindo, presidente!");
-                // TODO: implementar as opera��es dispon�veis para o Presidente
-                continue;
-            }
-
-            System.out.println("CPF e/ou senha inv�lidos.");
-        }
-
-        // escrever arquivo de texto com dados atualizados dos clientes, contas e funcion�rios
     }
-
-    private static Gerente verificarGerente(String cpf, String senha) {
-        // implementar a l�gica de verifica��o de Gerente
-        return null;
-    }
-
-    private static Diretor verificarDiretor(String cpf, String senha) {
-        // implementar a l�gica de verifica��o de Diretor
-        return null;
-    }
-
-    private static Presidente verificarPresidente(String cpf, String senha) {
-        // implementar a l�gica de verifica��o de Presidente
-        return null;
+    
+    public static void menu() {
+    	Scanner read = new Scanner(System.in);
+    	int escolha = 0;
+    	
+    	System.out.println("------------------------------------------------------------------");
+		System.out.println("---------------------   Sistema Bancário  ------------------------");
+		System.out.println("------------------------------------------------------------------\n");
+				
+		while (escolha != 3) {
+			System.out.println("1 - Login Cliente\n");
+			System.out.println("2 - Login Funcionário\n");
+			System.out.println("3 - Encerrar sistema");
+			
+			
+			escolha = read.nextInt();
+			
+			switch (escolha) {
+			case 1:
+				//menuCliente();
+				System.out.println("Cliente");
+				break;
+			case 2:
+				//menuFuncionario();
+				System.out.println("Funcionário");
+				break;
+			case 3:
+				System.out.println("Sistema encerrado!");
+				break;
+			default:
+				System.out.println("Opção inválida! \n ");
+				break;
+			
+			}
+		}
+		
+		
+		
+		
     }
 }
